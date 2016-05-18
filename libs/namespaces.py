bfa80caae95d7072dc17e2f56c30db64fcdbb81d
Frank <knarfytrebil@gmail.com>
2016-05-19 00:05:34 +0800

    fix

--- libs/namespaces.py
+++ libs/namespaces.py
@@ -11,32 +11,32 @@ class CoinNamespace(BaseNamespace):
         self.parse_args(args, 'global')
 
     def on_trades(self, *args):
-	self.parse_args(args, 'trade')
+        self.parse_args(args, 'trade')
 
     def parse_args(self, args, msg_type):
-	if msg_type == 'trade':
+        if msg_type == 'trade':
             msg = args[0]['message']
             detail = msg['msg']
-	    coin = msg['coin']
-	    self.parse_coin(detail, coin)
+        coin = msg['coin']
+        self.parse_coin(detail, coin)
 
     def parse_coin(self, coin_msg, coin):
-	if coin in WATCH_LIST:
-	    perc = float(coin_msg['perc'])
-	    vwapData = coin_msg['vwapData']
-	    cap24hrChange = float(coin_msg['cap24hrChange'])
-            supply = int(coin_msg['supply'])
-            vwapDataBTC = float(coin_msg['vwapDataBTC'])
-	    price = coin_msg['price']
-	    volume = float(coin_msg['volume'])
-            usdVolume = float(coin_msg['usdVolume'])
-	    mktcap = float(coin_msg['mktcap'])
-            TEMPLATE = BTC_TEMPLATE
-            SET =  (Fore.RED, coin, Fore.BLUE, price, Fore.GREEN, vwapData)
-            if coin != 'BTC':
-                delta = float(coin_msg['delta'])
-                cap24hrChangePercent = float(coin_msg['cap24hrChangePercent'])
-                capPercent = float(coin_msg['capPercent'])
-	        TEMPLATE = ALT_TEMPLATE
-	        SET = (Fore.RED, coin, capPercent, Fore.BLUE, price, Fore.GREEN, vwapData, Fore.YELLOW, delta)
-	    print TEMPLATE % SET
+    if coin in WATCH_LIST:
+        perc = float(coin_msg['perc'])
+        vwapData = coin_msg['vwapData']
+        cap24hrChange = float(coin_msg['cap24hrChange'])
+        supply = int(coin_msg['supply'])
+        vwapDataBTC = float(coin_msg['vwapDataBTC'])
+        price = coin_msg['price']
+        volume = float(coin_msg['volume'])
+        usdVolume = float(coin_msg['usdVolume'])
+        mktcap = float(coin_msg['mktcap'])
+        TEMPLATE = BTC_TEMPLATE
+        SET =  (Fore.RED, coin, Fore.BLUE, price, Fore.GREEN, vwapData)
+        if coin != 'BTC':
+            delta = float(coin_msg['delta'])
+            cap24hrChangePercent = float(coin_msg['cap24hrChangePercent'])
+            capPercent = float(coin_msg['capPercent'])
+            TEMPLATE = ALT_TEMPLATE
+            SET = (Fore.RED, coin, capPercent, Fore.BLUE, price, Fore.GREEN, vwapData, Fore.YELLOW, delta)
+        print TEMPLATE % SET
