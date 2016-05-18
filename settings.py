556162686423379cb3738acea9520eb66af985ce
Deploy Server <deployserver@deployserver.local>
2016-05-18 18:30:42 +0800

    fix

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
 WATCH_LIST = ['ETH', 'BTC', 'SC']
 PARAMS = ['perc', 'vwapData', 'cap24hrChange', 'price']
-ALT_TEMPLATE = "%s[%s][%sper] %s [PRICE][%s] %s [VWAP][%s] %s [DELTA]%s"
-BTC_TEMPLATE = "%s[%s] %s [PRICE][%s] %s [VWAP][%s]"
+ALT_TEMPLATE = "%s[%s][%sper]%s[PRICE][%s]%s[VWAP][%s]%s[DELTA]%s"
+BTC_TEMPLATE = "%s[%s]%s[PRICE][%s]%s[VWAP][%s]"
