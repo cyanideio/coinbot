c3cb45c79f9a67f2e3ab29136882650723798a7f
Deploy Server <deployserver@deployserver.local>
2016-05-18 11:56:57 +0800

    fix

--- libs/namespaces.py
+++ libs/namespaces.py
@@ -1,8 +1,23 @@
 from socketIO_client import BaseNamespace
+import json
 class CoinNamespace(BaseNamespace):
+
     def on_connect(self):
         print('[Connected]')
+
     def on_global(self, *args):
-        print('on_global', args)
+        self.parse_args(args, 'global')
+
     def on_trades(self, *args):
-        print('on_trade', args)
+	self.parse_args(args, 'trade')
+
+    def parse_args(self, args, msg_type):
+	if msg_type == 'trade':
+            msg = args[0]['message']
+            detail = msg['msg']
+	    coin = msg['coin']
+	    self.parse_coin(detail, coin)
+
+    def parse_coin(self, coin_msg, coin):
+	if coin == 'ETH':
+            print coin_msg
