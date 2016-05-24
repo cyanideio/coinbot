efed73ca3e27c442e51f1f233ed71c1fa2c05610
Frank <knarfytrebil@gmail.com>
2016-05-24 16:15:05 +0800

    fix

--- utils/db/models.py
+++ utils/db/models.py
@@ -10,7 +10,12 @@ class BaseModel(Model):
         database = db
 
 class Transaction(BaseModel):
-	coin_type = CharField()      # Type of the Virtual Coin
+	coinType = CharField()       # Type of the Virtual Coin
 	market = CharField()	     # Market on which the coin is traded
 	price =  DoubleField()       # Unit Price of the trade
 	volume = DoubleField()       # Volume of the Trade
+	vWap = DoubleField()         # Volume weighted price based on 24hours of trading data on all exchanges
+	vWapBTC = DoubleField()      # vWap by BTC
+	marketCap = DoubleField()    # Market Capitalization, total value of the coin
+	timestamp = DateTimeField(   # Now
+		default = datetime.datetime.now) 
