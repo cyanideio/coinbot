9f0d00b56afb9905d9a7115d50d7c81ff2bd3e6f
Frank <knarfytrebil@gmail.com>
2016-05-24 19:12:37 +0800

    modify models

--- run.py
+++ run.py
@@ -5,12 +5,12 @@ from colorama import init as color_init
 from socketIO_client import SocketIO
 from settings import COIN_CAP_HOST, COIN_CAP_PORT
 from libs.namespaces import CoinNamespace
-from utils.db.models import db, Transaction
+from utils.db.models import db, CoinCapBTCTrans, CoinCapAltTrans
 
 # Initialize Color Library
 color_init()
 db.connect()
-db.create_tables([CoinCapTransaction])
+db.create_tables([CoinCapBTCTrans, CoinCapAltTrans])
 
 coin_socketIO = SocketIO(COIN_CAP_HOST, COIN_CAP_PORT, CoinNamespace)
 coin_socketIO.wait()
--- utils/db/models.py
+++ utils/db/models.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 from settings import DB
 from peewee import *
 from playhouse.sqlite_ext import SqliteExtDatabase
@@ -10,15 +12,23 @@ class BaseModel(Model):
         database = db
 
 class CoinCapTransaction(BaseModel):
-	percent = DoubleField()      # Price Change in Percentage
-	coinType = CharField()       # Type of the Virtual Coin
-	market = CharField()	     # Market on which the coin is traded
-	supply = DoubleField() 		 # Supply
-	cap24hrChange = FloatField() # Cap Change in 24 Hours
-	price =  DoubleField()       # Unit Price of the trade
-	volume = DoubleField()       # Volume of the Trade
-	vWap = DoubleField()         # Volume weighted price based on 24hours of trading data on all exchanges
-	vWapBTC = DoubleField()      # vWap by BTC
-	marketCap = DoubleField()    # Market Capitalization, total value of the coin
-	timestamp = DateTimeField(   # Now
-		default = datetime.datetime.now) 
+    market = CharField()         # Market on which the coin is traded
+    supply = DoubleField()       # Supply
+    cap24hrChange = FloatField() # Cap Change in 24 Hours
+    price =  DoubleField()       # Unit Price of the trade
+    volume = DoubleField()       # Volume of the Trade
+    vWAP = DoubleField()         # Volume weighted price based on 24hours of trading data on all exchanges
+    vWAPBTC = DoubleField()      # vWap by BTC
+    marketCap = DoubleField()    # Market Capitalization, total value of the coin
+    timestamp = DateTimeField(   # Now
+        default = datetime.datetime.now) 
+
+class CoinCapBTCTrans(CoinCapTransaction):
+    pass
+
+class CoinCapAltTrans(CoinCapTransaction):
+    coinType = CharField()       # Type of the Virtual Coin
+    delta = DoubleField()        # DeltaPrice
+    percent = DoubleField()      # Price Change in Percentage
+    cap24hrChangePercent = FloatField()
+    capPercent = FloatField()
