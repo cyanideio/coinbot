995e03373c33f70f032c5e12d54e9b5e4baa1ee4
Frank <knarfytrebil@gmail.com>
2016-06-11 21:37:34 +0800

    fix

--- utils/db/models.py
+++ utils/db/models.py
@@ -6,7 +6,7 @@ from playhouse.sqlite_ext import SqliteExtDatabase
 from playhouse.postgres_ext import PostgresqlExtDatabase
 import datetime
 
-db = PostgresqlExtDatabase(DB, user='coin_watcher')
+db = PostgresqlExtDatabase(DB, user='coin_watcher', register_hstore=False)
 user_db = SqliteExtDatabase(USERDB)
 
 ##################
@@ -31,39 +31,39 @@ class User(Model):
 ##################
 class BaseModel(Model):
     coinType = CharField()       # Type of the Virtual Coin
-    price =  DoubleField()       # Unit Price of the trade
-    volume = DoubleField()       # Volume of the Trade
+    price =  FloatField()       # Unit Price of the trade
+    volume = FloatField()       # Volume of the Trade
     timestamp = DateTimeField(   # Now
         default=datetime.datetime.now) 
     class Meta:
         database = db
 
 class CoinCapTransaction(BaseModel):
-    percent = DoubleField()      # Price Change in Percentage
-    supply = DoubleField()       # Supply
+    percent = FloatField()      # Price Change in Percentage
+    supply = FloatField()       # Supply
     cap24hrChange = FloatField() # Cap Change in 24 Hours
-    vWAP = DoubleField()         # Volume weighted price based on 24hours of trading data on all exchanges
-    vWAPBTC = DoubleField()      # vWap by BTC
-    marketCap = DoubleField()    # Market Capitalization, total value of the coin
+    vWAP = FloatField()         # Volume weighted price based on 24hours of trading data on all exchanges
+    vWAPBTC = FloatField()      # vWap by BTC
+    marketCap = FloatField()    # Market Capitalization, total value of the coin
 
 class CoinCapBTCTrans(CoinCapTransaction):
     pass
 
 class CoinCapAltTrans(CoinCapTransaction):
-    delta = DoubleField()        # DeltaPrice
+    delta = FloatField()        # DeltaPrice
     cap24hrChangePercent = FloatField()
     capPercent = FloatField()
 
 class YunbiTrans(BaseModel):
-    sell = DoubleField()
-    buy = DoubleField()
-    high = DoubleField()
-    low = DoubleField()
+    sell = FloatField()
+    buy = FloatField()
+    high = FloatField()
+    low = FloatField()
 
 class PoloniexTrans(BaseModel):
-    percent = DoubleField()      # Price Change in Percentage
-    lowestAsk = DoubleField()
-    highestBid = DoubleField()
-    quoteVolume = DoubleField()
-    mkt24hrHigh = DoubleField()
-    mkt24hrLow = DoubleField()
+    percent = FloatField()      # Price Change in Percentage
+    lowestAsk = FloatField()
+    highestBid = FloatField()
+    quoteVolume = FloatField()
+    mkt24hrHigh = FloatField()
+    mkt24hrLow = FloatField()
